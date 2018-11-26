32e5567685def28351688e25549951562b823941
Conrad <conrad.heidebrecht@gmail.com>
2018-11-26 09:53:31 -0500

    minor changes

--- lib/data/game_state.dart
+++ lib/data/game_state.dart
@@ -8,35 +8,21 @@ enum GameStatus {
 
 class GameState {
 
-  factory GameState({
-    Size windowSize = const Size(1.0, 1.0),
-    double gravity = -0.1,
-    GameStatus status,
-  }) {
-    _instance ??= GameState._internal(windowSize: windowSize, gravity: gravity, status: status);
-    return _instance;
-  }
-
-  GameState._internal({
+  GameState({
     this.windowSize = const Size(1.0, 1.0),
     this.gravity = -0.1,
-    this.status = GameStatus.START
+    this.status = GameStatus.START,
   });
 
-  void copyWith({
+  GameState copyWith({
     Size windowSize,
     double gravity,
     GameStatus status
-  }) {
-    var newState = GameState(
-      windowSize: windowSize ?? _instance.windowSize,
-      gravity: gravity ?? _instance.gravity,
-      status: status ?? _instance.status
-    );
-
-  }
-
-  static GameState _instance;
+  }) => GameState(
+    windowSize: windowSize ?? this.windowSize,
+    gravity: gravity ?? this.gravity,
+    status: status ?? this.status,
+  );
 
   Size windowSize;
   double gravity;
@@ -61,16 +47,12 @@ class GameState {
 
 }
 
-class GameStateObservable {
-
-  static ValueNotifier<GameState> _observable;
-  static ValueNotifier<GameState> get observable => _observable;
-  static set observable(dynamic newVal) {
-    if (newVal is ValueNotifier<GameState> && _observable == null) {
-      _observable = newVal;
-    } else if (newVal is GameState && _observable != null) {
-      _observable.value = newVal;
-    }
+ValueNotifier<GameState> _gameObservable;
+ValueNotifier<GameState> get gameObservable => _gameObservable;
+set gameObservable(dynamic newVal) {
+  if (newVal is ValueNotifier<GameState> && _gameObservable == null) {
+    _gameObservable = newVal;
+  } else if (newVal is GameState && _gameObservable != null) {
+    _gameObservable.value = newVal;
   }
-
 }
